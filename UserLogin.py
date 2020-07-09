f7ded13a595cb9636408b503ecded64ac1b00c2a
D-P1R5N <54560169+D-P1R5N@users.noreply.github.com>
2020-07-08 23:07:19 -0400

    Update UserLogin.py

--- UserLogin.py
+++ UserLogin.py
@@ -1,88 +1,84 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 from tkinter.font import Font
 from tkinter import ttk
 import sqlite3
 
-class Keywin(Tk):
+class Keywin(tk.Tk):
     def __init__(self):
-        Tk.__init__(self)
+        tk.Tk.__init__(self)
         self.title("Login")
         self.resizable(False,False)
         self.overrideredirect(True)
         self.geometry("+300+300")
         self.pin = str()
-        self.entry = self.Pin(self)
+
+        self.entry = ttk.Entry(width=15)
         self.entry.grid(column=0,row=0, pady=2)
-        self.numpad = self.Numpad(self).grid(column=0,row=1)
-        self.destuct = self.destructButton(self).grid(column=0, row=4, ipady=5, pady=2)
-    def close():
-        window.destroy()
 
-    def code(Keywin, value):
-        entry = Keywin.entry
-        if value == 'Del':
-            Keywin.pin = Keywin.pin[:-1]
-            entry.delete('0', 'end')
-            entry.insert('end', Keywin.pin)
-        elif value == 'Enter' :
-            conn = sqlite3.connect('employee.db')
-            cur = conn.cursor()
-            user = entry.get()
-            cur.execute('SELECT * FROM EMP WHERE Pass= ?', (entry.get(),))
-            employee = cur.fetchone()
-            if not employee:
-                print("The PIN was incorrect")
-                entry.delete('0', 'end')
-                Keywin.pin = str()
-                entry.insert('end', Keywin.pin)
-            else:
-                print("Welcome", employee[0])
-                conn.close()
-                entry.delete('0', 'end')
-                Keywin.pin = str()
-                entry.insert('end', Keywin.pin)
-        else:
-            if len(Keywin.pin) < 4:
-                Keywin.pin += str(value)
-                entry.insert('end', value)
-            else: pass
+        self.numpad = self.Numpad()
+        self.numpad.grid(column=0,row=1,padx=5,pady=2)
+
+        self.destruct = ttk.Button(command=self.close,text="Close",width=15)
+        self.destruct.grid(column=0, row=4, ipady=5, pady=(2,5))
+
+    def close(self):
+        self._root().destroy()
+
     class Numpad(ttk.Frame):
-        def __init__(self, root):
-            ttk.Frame.__init__(self,root)
-            self.root = root
-            self.grid()
-            self.numpad_create()
+        def __init__(self):
+            ttk.Frame.__init__(self)
+            self.root = self._root()
+            self.numpad_create([*range(1,10), *['Del','0','Enter']])
 
-        def numpad_create(self):
+        def numpad_create(self,nums):
             r,c = 1,0
-            nums = [*range(1,10), *['Del','0','Enter']]
+
             for b in nums:
-                cmd = lambda button = b: Keywin.code(self.root, button)
-                self.b = Button(self, text=b,width=15,command=cmd, background = 'honeydew', foreground= 'blue')
-                self.b.font = Font(family = 'Arial Baltic', size = 12, weight= 'bold')
-                self.b.configure(font=self.b.font,activebackground='DarkGreen')
-                self.b.grid(row=r,column=c,ipady=20, sticky=(N,E,S,W))
+                cmd = lambda x=b: self.code(x)
+                self.b = ttk.Button(self, text=b,width=15,command=cmd)
+                self.b.grid(row=r,column=c,ipady=20, sticky='N,E,S,W')
                 c += 1
                 if c>= 3:
                     c=0
                     r+=1
-    class Pin(ttk.Entry):
-        def __init__(self,root):
-            ttk.Entry.__init__(self, root)
-            self.width = 15
-            self.font = Font(family = "Franklin Gothic Medium", size = 18)
-            self.configure(font=self.font, justify = CENTER)
-            self.textvariable = StringVar()
-    class destructButton(Button):
-        def __init__(self,parent):
-            Button.__init__(self,parent)
-            self['text'] = "Close"
-            self['command'] = parent.destroy
-            self['width'] = 27
-            self['bg'] = 'orangered'
-            self['activebackground'] ='red2'
-            self.font = Font(family = "Bahnschrift", size = 12, weight = "bold")
-            self.configure(font=self.font)
+
+        def code(self, value):
+            entry = self.root.entry
+            if value == 'Del':
+                self.root.pin = self.root.pin[:-1]
+                entry.delete('0', 'end')
+                entry.insert('end', self.root.pin)
+
+            elif value == 'Enter' :
+                pin = entry.get()
+                try:
+                    with sqlite3.connect('employee.db') as conn:
+                    #There should only be one EMPLOYEE per PIN
+                        cur = conn.cursor()
+                        cur.execute('SELECT * FROM EMP WHERE Pass= ?', (pin,))
+                        employee = cur.fetchone()
+
+                        if not employee:
+                            print("The PIN was incorrect")
+                            entry.delete('0', 'end')
+                            self.root.pin = str()
+                            entry.insert('end', self.root.pin)
+
+                        else:
+                            print("Welcome", employee[0])
+                            entry.delete('0', 'end')
+                            self.root.pin = str()
+                            entry.insert('end', self.root.pin)
+                            
+                except sqlite3.OperationalError as e:
+                    _err = messagebox.showerror("Operational Error", e)
+            else:
+                if len(self.root.pin) < 4:
+                    self.root.pin += str(value)
+                    entry.insert('end', value)
+                else: pass
+
 if __name__ == "__main__":
 
     keypad = Keywin()
